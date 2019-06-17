f129164f313a296b17989cc7b38bdd54e8518f79
edanielbluford <e.daniel.bluford@outlook.com>
2019-06-17 15:05:02 -0400

    Finished challenge one

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "My Name is Ernest.");
+//fs.writeFileSync("notes.txt", "My Name is Ernest.");
+
+//Challenge Append a message to notes.txt
+//
+//1. Use appendFileSync to append the file.
+//2. Run the Script
+//3. Check your work.
+
+fs.appendFileSync("notes.txt", "Learning Node.js!");
